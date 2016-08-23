d856be62dcdd0035a3892ace0691b4804af46847
riazuddin <ruuddin@yahoo.com>
2016-08-22 19:00:28 -0700

    test

--- .lpvenv/03scopeExamples.py
+++ .lpvenv/03scopeExamples.py
@@ -8,31 +8,45 @@ print 'global:', test
 
 # Example: Function inside a function
 #-- local and outer scope variables
-def outer():
+def f2_outer():
     test = 1
-    def inner():
+    def f2_inner():
         test = 2
         print 'inner:', test
     
-    inner()
+    f2_inner()
     print 'outer:', test #if line 12 is commented, this will print value of line 20
 
 test = 0 #global scope
-outer()
+f2_outer()
 print 'global:', test
 
 # 'nonlocal' Example: how to use variable from outer scope in inner scope
-def outer():
-    test = 1
-    def inner():
-        nonlocal test #user scope from line 26
-        test = 2 #changes outer scope value of test to 2
-        print 'inner:', test
+def f3_outer():
+    test2 = 1
+    def f3_inner():
+        nonlocal test2
+        test2 = 2 #changes outer scope value of test to 2
+        print 'inner:', test2
     
-    inner()
-    print 'outer:', test #outer scope
+    f3_inner()
+    print 'outer:', test2 #outer scope
 
-test = 0 #global scope
-outer()
-print 'global:', test
+test2 = 0 #global scope
+f3_outer()
+print 'global:', test2
 
+# 'global' - bind to variable in global scope
+def f4_outer():
+    test3 = 1  # outer scope
+
+    def f4_inner():
+        global test3
+        test1 = 2  # global scope
+        print 'inner:', test3
+    f4_inner()
+    print 'outer:', test3
+
+test3 = 0  # global scope
+f4_outer()
+print 'global:', test3
